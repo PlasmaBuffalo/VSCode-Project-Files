710175fc7bf193116381cbd9f0f2be9ef53991ee
Liam Z <zalu.liam@gmail.com>
2022-09-25 23:30:00 -0400

    deleted:    Python/CS420-DPC/1 on 1 Chat/client.py 	deleted:    Python/CS420-DPC/1 on 1 Chat/tcpServer.py 	new file:   Python/CS420-DPC/Project 1-3TierChat/TcpServerQt.py 	modified:   Python/ConwayGameOfLife/gol.py

--- Python/ConwayGameOfLife/gol.py
+++ Python/ConwayGameOfLife/gol.py
@@ -1,3 +1,9 @@
-#creating the starting grid
+#creating the grid
+
+#displaying the grid
 
 #every half second, generate updated grid:
+#for each cell:
+#   die if 0-1 neighbors or 4+ neighbors (overpopulation)
+#   live if 2-3 neighbors
+#   become alive if dead but has 3 neighbors
