91ce169174d91c1c477b57cad8e726e7e7a2fe35
Liam Z <zalu.liam@gmail.com>
2022-09-29 21:40:16 -0400

    new file:   Python/CS420-DPC/Project 1 - Chat/bank_mqtt.py 	new file:   Python/CS420-DPC/Project 1 - Chat/branch_mqtt.py 	new file:   Python/CS420-DPC/Project 1 - Chat/mqtt.py 	new file:   Python/CS420-DPC/Project 1 - Chat/start_branhces_mqtt.py 	deleted:    Python/CS420-DPC/Project 1-3TierChat/TcpServerQt.py

---
+++ Python/CS420-DPC/Project 1 - Chat/start_branhces_mqtt.py
@@ -0,0 +1,13 @@
+from subprocess import Popen, PIPE
+from multiprocessing import Process
+
+if __name__ == "__main__":
+
+	processes = []
+	for i in range(5):
+		proc = Popen(['python3','branch_mqtt.py',str(i)], stderr=PIPE, stdout=PIPE)
+		processes.append(proc)
+		
+	for proc in processes:
+		stderr, stdout = proc.communicate()
+		print(stderr, stdout)
