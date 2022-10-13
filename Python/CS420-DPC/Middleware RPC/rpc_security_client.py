58c5276e27f8a3affbe12d86c3105902f376f62b
Liam Z <zalu.liam@gmail.com>
2022-10-13 17:20:21 -0400

    new file:   .vscode/VSCode Project Files.code-workspace 	modified:   Python/CS420-DPC/Middleware RPC/rpc_security_client.py 	modified:   Python/CS420-DPC/Middleware RPC/simple_rpc_class_server.py 	modified:   Python/CS420-DPC/Middleware RPC/simple_rpc_client.py 	modified:   Python/CS420-DPC/Middleware RPC/simple_rpc_server.py 	new file:   Python/CS420-DPC/Project 1 is that easy/client.py 	new file:   Python/CS420-DPC/Project 1 is that easy/server.py

--- Python/CS420-DPC/Middleware RPC/rpc_security_client.py
+++ Python/CS420-DPC/Middleware RPC/rpc_security_client.py
@@ -4,15 +4,15 @@ import sys
 
 if __name__ == "__main__":
 
-	name = sys.argv[1]
-	item = sys.argv[2]
+    name = sys.argv[1]
+    item = sys.argv[2]
 
-	rpcClient = ServerProxy('http://169.254.213.62:8000')
-	
-	msg = rpcClient.isAllowedIn(name, item)
-	
-	isAllowed = True if msg['request'] == 'accepted' else False
-	print('{} is allowed in to {}: {}'.format(name,item,isAllowed))
-	
-	msg = rpcClient.allowedItems(name)
-	print(msg)
+    rpcClient = ServerProxy('http://169.254.213.62:8000')
+
+    msg = rpcClient.isAllowedIn(name, item)
+
+    isAllowed = True if msg['request'] == 'accepted' else False
+    print('{} is allowed in to {}: {}'.format(name, item, isAllowed))
+
+    msg = rpcClient.allowedItems(name)
+    print(msg)
--- Python/CS420-DPC/Middleware RPC/simple_rpc_server.py
+++ Python/CS420-DPC/Middleware RPC/simple_rpc_server.py
@@ -1,23 +1,26 @@
 from xmlrpc.server import SimpleXMLRPCServer
-from threading import Thread 
+from threading import Thread
+
 
 def add(num1, num2):
-	print('\nAdding numbers {} and {}'.format(num1,num2))
-	result = num1 + num2
-	return result
-	
+    print('\nAdding numbers {} and {}'.format(num1, num2))
+    result = num1 + num2
+    return result
+
+
 def subtract(num1, num2):
-	print('\nSubtracting numbers {} and {}'.format(num1,num2))
-	result = num1 - num2
-	return result
-	
+    print('\nSubtracting numbers {} and {}'.format(num1, num2))
+    result = num1 - num2
+    return result
+
+
 if __name__ == "__main__":
 
-	rpcServer = SimpleXMLRPCServer(('169.254.213.62',8000))
-	rpcServer.register_function(add)
-	rpcServer.register_function(subtract)
-	
-	thread = Thread(target=rpcServer.serve_forever, args=())
-	thread.start()
-	
-	print('RPC Server is running..')
+    rpcServer = SimpleXMLRPCServer(('169.254.213.62', 8000))
+    rpcServer.register_function(add)
+    rpcServer.register_function(subtract)
+
+    thread = Thread(target=rpcServer.serve_forever, args=())
+    thread.start()
+
+    print('RPC Server is running..')
