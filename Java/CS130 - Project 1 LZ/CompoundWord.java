9f4ab9c6c538afa8ba4f3a213cc56e44d45c76b2
PlasmaBuffalo <89225375+PlasmaBuffalo@users.noreply.github.com>
2022-11-09 13:21:49 -0500

    modified:   Java/CS130 - Project 1 LZ/CompoundWord.java

--- Java/CS130 - Project 1 LZ/CompoundWord.java
+++ Java/CS130 - Project 1 LZ/CompoundWord.java
@@ -6,7 +6,6 @@
 //import statements
 import java.util.Scanner;
 
-
 //import arraylist
 import java.util.ArrayList;
 
@@ -15,10 +14,10 @@ public class CompoundWord {
         // create instance of main class
         CompoundWord cw = new CompoundWord();
         // create some compound words and some simple words
-        Word word1 = new Word("starfish", 4, true, 1, new String[] {"animals", "ocean"});
-        Word word2 = new Word("toothbrush", 5, true, 1, new String[] {"hygiene", "tooth"});
-        Word word3 = new Word("cat", -1, false, 1, new String[] {"animals", "pets"});
-        Word word4 = new Word("dog", -1, false, 1, new String[] {"animals", "pets"});
+        Word word1 = new Word("starfish", 4, true, 1, new String[] { "animals", "ocean" });
+        Word word2 = new Word("toothbrush", 5, true, 1, new String[] { "hygiene", "tooth" });
+        Word word3 = new Word("cat", -1, false, 1, new String[] { "animals", "pets" });
+        Word word4 = new Word("dog", -1, false, 1, new String[] { "animals", "pets" });
 
         // print an explanation of what a compound word is
         System.out.println(
@@ -43,29 +42,32 @@ public class CompoundWord {
         Scanner input = new Scanner(System.in);
         String answer = input.nextLine();
 
-        //switch for each case if the word was compound or not and if the user said it was or not
+        // switch for each case if the word was compound or not and if the user said it
+        // was or not
         switch (answer) {
             case "y":
                 if (w.isCompound()) {
-                    //if word is compound and user said it was, print a message saying they were correct
+                    // if word is compound and user said it was, print a message saying they were
+                    // correct
                     System.out.println("Correct!");
                 } else {
-                    //correct the user
+                    // correct the user
                     System.out.println("Incorrect. The word " + w.getWord() + " is not a compound word.");
                 }
-                //call whereCompound method with the word as a parameter
+                // call whereCompound method with the word as a parameter
                 whereCompound(w);
                 break;
             case "n":
                 if (w.isCompound()) {
-                    //correct the user if they said the word was not compound when it is
+                    // correct the user if they said the word was not compound when it is
                     System.out.println("Incorrect! The word " + w.getWord() + " is a compound word!");
                 } else {
-                    //if word was not compound and user said it was not, print a message saying they were correct
+                    // if word was not compound and user said it was not, print a message saying
+                    // they were correct
                     System.out.println("Correct!");
                 }
                 break;
-            //in case the user does something stupid
+            // in case the user does something stupid
             default:
                 System.out.println("Invalid input!");
                 break;
@@ -73,22 +75,23 @@ public class CompoundWord {
     }
 
     public void whereCompound(Word w) {
-        //print out the w.getWord() print a number under each letter of w.getWord()
+        // print out the w.getWord() print a number under each letter of w.getWord()
         System.out.println(w.getWord());
         for (int i = 0; i < w.getWord().length(); i++) {
             System.out.print(i);
         }
-        //ask the user which number the second word starts with
+        // ask the user which number the second word starts with
         System.out.println("Which number does the second word start with in this compound word?");
-        //use a scanner to take user input for an int
+        // use a scanner to take user input for an int
         Scanner input = new Scanner(System.in);
         int answer = input.nextInt();
-        //if the answer is correct, print a message saying they were correct
+        // if the answer is correct, print a message saying they were correct
         if (answer == w.getIndex()) {
             System.out.println("Correct!");
-            //*END CASE
+            // *END CASE
         } else {
-            //if the answer is incorrect, print a message saying they were incorrect and what the correct answer was
+            // if the answer is incorrect, print a message saying they were incorrect and
+            // what the correct answer was
             System.out.println("Incorrect! The correct answer was " + w.getIndex() + ".");
         }
     }
