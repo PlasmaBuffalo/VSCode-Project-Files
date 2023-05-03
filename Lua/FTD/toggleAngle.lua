d1edd8eccaac0992c1f55018fb4c4dca1f981830
PlasmaBuffalo <89225375+PlasmaBuffalo@users.noreply.github.com>
2023-05-03 15:43:22 -0400

    update with edit variables

--- Lua/FTD/toggleAngle.lua
+++ Lua/FTD/toggleAngle.lua
@@ -1,12 +1,15 @@
 function Update(I)
+    local speedFactor = 4
+    local maxAngle = 45
+
     -- Get the current time in seconds
     local time = I:GetTime()
 
     -- Calculate the angle to use based on whether the time is even or odd
-    local angle = math.floor(time) % 2 == 0 and 45 or -45
+    local angle = math.floor(time*speedFactor) % 2 == 0 and maxAngle or -maxAngle
 
     -- Set the angle of the spin block with the given SubConstructIdentifier
-    I:SetSpinBlockRotationAngle(SubConstructIdentifier, angle)
+    I:SetSpinBlockRotationAngle(32, angle)
 
     -- Print a log statement to show the current time and angle being used
     I:Log(string.format("Time: %d, Angle: %d", time, angle))
