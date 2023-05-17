2359b05fd6498064377ca60053c9cd8b4c0075d7
PlasmaBuffalo <89225375+PlasmaBuffalo@users.noreply.github.com>
2023-05-17 16:24:55 -0400

    add timer framework

---
+++ Lua/FTD/timedFunction.lua
@@ -0,0 +1,14 @@
+-- after an indicated period of time passes, the function will be called again
+-- amount of time to take between function calls (in seconds)
+local loopInterval = 10
+
+function Timer(I)
+    -- get the current time
+    local time = Mathf.floor(I:GetTime() % loopInterval)
+    -- whenever time is zero, return true
+    if time == 0 then
+        return true
+    else
+        return false
+    end -- close if statement
+end -- close function
