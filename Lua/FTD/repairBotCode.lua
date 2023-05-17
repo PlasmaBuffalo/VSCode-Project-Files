dd1d42fbb41909626dd2f9b181d99dcba5d33eb6
PlasmaBuffalo <89225375+PlasmaBuffalo@users.noreply.github.com>
2023-05-17 16:31:53 -0400

    add code specifically for repairbot 1

---
+++ Lua/FTD/repairBotCode.lua
@@ -0,0 +1,23 @@
+-- loop through friendly health values
+-- pick the lowest friendly health fraction and go heal that friendly until health fraction is 1.0
+-- repeat
+-- after an indicated period of time passes, the function will be called again
+-- amount of time to take between function calls (in seconds)
+local loopInterval = 10
+
+function Timer()
+    -- get the current time
+    local time = Mathf.floor(I:GetTime() % loopInterval)
+    -- whenever time is zero, return true
+    if time == 0 then
+        return true
+    else
+        return false
+    end -- close if statement
+end -- close function
+
+function Update(I)
+    if Timer() then
+        I:Log("Timer() returned true")
+    end
+end
