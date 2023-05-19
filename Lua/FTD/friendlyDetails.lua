c74511d1c8dc32f842d8baedec81d58c3e149d63
PlasmaBuffalo <89225375+PlasmaBuffalo@users.noreply.github.com>
2023-05-19 13:39:53 -0400

    update friendlyDetails program

--- Lua/FTD/friendlyDetails.lua
+++ Lua/FTD/friendlyDetails.lua
@@ -1,11 +1,21 @@
 -- A program to constantly log the health fraction of all friendly vehicles
-function Update(I)
+function FriendlyHealth(I)
 
+    -- number of friendlies on the field
     local friendlyCount = I:GetFriendlyCount()
+    -- variable to store lowest health fraction found in list
+    local lowestHealth = 2
+    -- index of the friendly with the lowest health fraction
+    local lowestHealthIndex = -1
 
     for index = 0, friendlyCount - 1 do
         local friendlyInfo = I:GetFriendlyInfo(index)
         local healthFraction = friendlyInfo:Get("HealthFraction")
+        if healthFraction < lowestHealth then
+            lowestHealth = healthFraction
+            lowestHealthIndex = index
+        end -- close if statement
         I:Log(string.format("Friendly %d - Health Fraction: %.2f", index, healthFraction))
     end -- close for loop
-end -- close Update function
+    return lowestHealthIndex
+end -- close function
