92582a6d0c6bbb808d6eba4a2339ff5cbaa9c00a
PlasmaBuffalo <89225375+PlasmaBuffalo@users.noreply.github.com>
2023-06-08 16:19:56 -0400

    updating positioning

--- Lua/FTD/dynamicGeographicMap.lua
+++ Lua/FTD/dynamicGeographicMap.lua
@@ -3,13 +3,17 @@
 local stepDistance = 100
 -- zeroPoint: the local coordinates for the central piston, placing the ship at the center of the map at all times
 -- this value is set by user, or else the position differences will be incorrect
-local zeroPoint = Vector3(0, 0, 0)
+local zeroPoint = Vector3.zero
+local terrainBounds = {
+    lower = -250,
+    upper = 250
+}
 
 function Update(I)
     local componentCount = I:GetAllSubconstructsCount()
     for i = 0, componentCount, 1 do
         -- only iterating on the pistons, ignore others subconstructs
-        if I:IsPiston(I:GetSubConstructIdentifier(i)) and (I:GetSubConstructInfo(I:GetSubConstructIdentifier(i)).Valid) then
+        if I:GetSubConstructInfo(I:GetSubConstructIdentifier(i)).Valid and I:IsPiston(I:GetSubConstructIdentifier(i)) then
             -- get difference in x/z coordinates between the current piston and the zeroPoint
             local pistonInfo = I:GetSubConstructInfo(I:GetSubConstructIdentifier(i))
 
@@ -19,17 +23,15 @@ function Update(I)
                 I:Log("zeroPoint is now " .. zeroPoint.x .. ", " .. zeroPoint.y .. ", " .. zeroPoint.z)
             end
 
-            -- print out the local position of the piston
-            I:Log(
-                "localPos of ID=" .. pistonInfo.SubConstructIdentifier .. " is " .. pistonInfo.LocalPosition.x .. ", " ..
-                    pistonInfo.LocalPosition.y .. ", " .. pistonInfo.LocalPosition.z)
-
             -- difference between piston positions: pInfo.LocalPosition - zeroPoint
             local pistonPosDiff = pistonInfo.LocalPosition - zeroPoint
-            I:Log("pistonPosDiff is " .. pistonPosDiff.x .. ", " .. pistonPosDiff.y .. ", " .. pistonPosDiff.z)
-
+            I:Log("relative location for ID" .. pistonInfo.SubConstructIdentifier .. " is " ..pistonInfo.LocalPosition.x .. ", " .. pistonInfo.LocalPosition.y .. ", " .. pistonInfo.LocalPosition.z)
+            I:Log("pistonPosDiff for ID" .. pistonInfo.SubConstructIdentifier .. " is " .. pistonPosDiff.x .. ", " ..
+                      pistonPosDiff.y .. ", " .. pistonPosDiff.z)
             -- multiply by stepDistance to get the proper point on terrain to checking
             local terrainPosDiff = pistonPosDiff * stepDistance
+            I:Log("terrainPosDiff for ID" .. pistonInfo.SubConstructIdentifier .. " is " .. terrainPosDiff.x .. ", " ..
+                      terrainPosDiff.y .. ", " .. terrainPosDiff.z)
             -- get terrain altitude at this point
             -- set piston height according to terrain altitude
         end -- close if statement
