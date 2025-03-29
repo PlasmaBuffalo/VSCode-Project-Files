c9d46efc4e8dc8fd45f1727eb89628054599ab8d
PlasmaBuffalo <89225375+PlasmaBuffalo@users.noreply.github.com>
2025-03-29 15:42:36 -0400

    Autograb is COMPLETE AND FUNCTIONAL

--- Lua/FTD/TractorBeamAutoGrab.lua
+++ Lua/FTD/TractorBeamAutoGrab.lua
@@ -19,30 +19,35 @@ function NameScan(I, nameString)
         --I:Log(string.format("Friendly Name %s - Trim Name %s", friendlyName, trimName))
 
         --if they are equal now, we have our target.
-        I:Log(string.format("Comparing %s and %s", trimName, nameString))
+        --I:Log(string.format("Comparing %s and %s", trimName, nameString))
         if trimName == nameString and friendlyInfo.Valid then
-            I:Log(string.format("Confirmed match between %s and %s", trimName, nameString))
+            --I:Log(string.format("Confirmed match between %s and %s", trimName, nameString))
             --idk which one it should be here
+            --I:Log(string.format("friendlyInfoId is %d, index is %d",friendlyInfo.Id, index))
             targetConstructIndex = friendlyInfo.Id
             --targetConstructIndex = index
+            --I:Log("Returning "..targetConstructIndex.."(NOT be -1)")
             return targetConstructIndex
         end
         --this function should return the unique id of this friendly found above
     end -- close for loop
     --this return should only ever return -1
-    I:Log("Returning "..targetConstructIndex)
+    --I:Log("Returning "..targetConstructIndex.."(should be -1)")
     return targetConstructIndex
 end -- close function
 
 function Update(I)
+    I:ClearLogs()
     I:Log("-START-")
     --loop through all tractor beams on the construct
     local tractorBeamCount = I:Component_GetCount(7)
-    I:Log("BeamCount="..tractorBeamCount)
+    --I:Log("BeamCount="..tractorBeamCount)
     for i = 0, tractorBeamCount, 1 do
         --get the custom name of the current tractor beam
         local beamInfo = I:Component_GetBlockInfo(7,i)
         local beamName = beamInfo.CustomName
+        -- here we will make the beam tell us what the ID of its current grab is
+        I:Log(string.format("beamName=%s, currentTargetIndex=%d",beamName, I:Component_GetIntLogic(7, i)))
 
         -- if we have a Target Set, bind this tractor beam to the first TS construct friendly
         if beamName == "Target Set" then
@@ -50,9 +55,12 @@ function Update(I)
             --search through the list of friendly constructs
             --find one with "Target Set" in the name
             local beamTargetIndex = NameScan(I, "PTP Target Set")
-            --tell this tractor beam to grab
+            --tell this tractor beam to grab the particular construct
             I:Component_SetIntLogic_1(7, i, 0, beamTargetIndex)
-            I:Log(string.format("Set component type %d"))
+            I:Log(string.format("Component type 7, blockIndex %d, propIndex 0, beamTargetIndex %d",i, beamTargetIndex))
+            --then turn the beam on
+            I:Component_SetBoolLogic(7,beamTargetIndex,true)
+
             --type=7 for tractor beam
             --blockIndex= index of the tractor beam we're working with
             --propertyIndex1=index of the component you want to set int logic for
@@ -65,6 +73,8 @@ function Update(I)
             local beamTargetIndex = NameScan(I, "PTP Weapon")
             --tell this tractor beam to grab it
             I:Component_SetIntLogic_1(7, i, 0, beamTargetIndex)
+            --then turn the beam on
+            I:Component_SetBoolLogic(7,beamTargetIndex,true)
         else
             I:Log("oh god, forget it")
         end
